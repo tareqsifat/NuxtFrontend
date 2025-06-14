52ce4b954ed19a6c1598182cede888a4aa076fe8
unknown <mdtareq.me@gmail.com>
2025-06-14 22:17:29 +0600

    added Auth.vue

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="">
+        <main>
+            <slot />
+        </main>
+        <AuthFooter />
+    </div>
+</template>
+<script>
+useHead({
+  link: [
+    { rel: 'stylesheet', href: '/css/auth.css' }
+  ]
+})
+</script>
