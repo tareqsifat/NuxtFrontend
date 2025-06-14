8f72fd953bbf2cfdd9236c1013bd54b7c4ff135f
unknown <mdtareq.me@gmail.com>
2025-06-14 22:19:31 +0600

    added login.vue

---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+    <h3 >Login</h3>
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth'
+});
+
+const form = reactive({ email: '', password: '' })
+const error = ref('')
+const loading = ref(false)
+const { login } = useAuth()
+
+async function handleLogin() {
+  error.value = ''
+  loading.value = true
+  try {
+    await login(form)
+    await navigateTo('/')
+  } catch (err: any) {
+    error.value = err
+  } finally {
+    loading.value = false
+  }
+}
+</script>
