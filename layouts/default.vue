3fb0727cfb3bcda829343eb003103f4d89ba80cf
unknown <mdtareq.me@gmail.com>
2025-06-14 22:17:41 +0600

    added default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { user, logout } = useAuth()
+</script>
+
+<template>
+  <header>
+    <template v-if="user">
+      <div>Welcome, {{ user?.name }}</div>
+      <button @click="logout()">Logout</button>
+    </template>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
