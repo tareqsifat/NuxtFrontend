13efbd0262d595b5fbba7f140df57c6263016f80
unknown <mdtareq.me@gmail.com>
2025-06-15 22:16:31 +0600

    added footer.vue

---
+++ components/Auth/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="bg-dark text-light text-center py-3">
+        &copy; {{ currentYear }} Boraq HRM. All rights reserved.
+    </footer>
+</template>
+
+<script setup>
+    const currentYear = new Date().getFullYear();
+</script>
+
+<style scoped>
+    footer {
+        background-color: #111;
+        color: #bbb;
+    }
+</style>
